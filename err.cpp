7ef5cb0ea78b5c17b9946ff6f70fea48cc80b732
Devil-Chan <35504477+Devil-Chan@users.noreply.github.com>
2018-03-19 13:45:36 -0600

    Add files via upload

---
+++ err.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main() {
+	while (1)
+		MessageBox(NULL, ("YOU DIDN'T LITSENT TO DEADPOOL, YOU BROKE THIS COMPUTER WITH YOU DUMBNESS >:("), ("YOU BUTT HEAD >:("), MB_OK | MB_SYSTEMMODAL);
+}
