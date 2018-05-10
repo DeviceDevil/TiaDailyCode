c3130b6f1270d86791cb2126456c7900775fe06d
Devil-Chan <35504477+Devil-Chan@users.noreply.github.com>
2018-05-10 13:54:11 -0600

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int sum;
+	int t;
+	int input;
+	cout << "can you ples give me a number? :3" << endl;
+	cin >> input;
+	
+	for (int t = 0; t < input; t++) {
+		sum = t;
+		sum++;
+		cout << sum;
+		
+		cout << sum + sum + sum + sum + sum << sum << endl;
+	}
+	
+	//what would brian boitano do???
+}
